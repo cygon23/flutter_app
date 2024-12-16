201e689f07f271e363bff9f573edda580f14ea2a
cygon23 <rweyemamu088@gmail.com>
2024-12-16 18:37:34 +0300

    basic navigation complete

---
+++ lib/screens/bottom_navigation/basic_navigation.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_ui/model/bottom_nav.dart';
+
+class BasicNavigation extends StatefulWidget {
+  const BasicNavigation();
+
+  @override
+  _BasicNavigationState createState() => _BasicNavigationState();
+}
+
+class _BasicNavigationState extends State<BasicNavigation> {
+  final List<BottomNav> itemsNav = <BottomNav>[
+    BottomNav('Home', Icons.store, null),
+    BottomNav('Business', Icons.business, null),
+    BottomNav('Profile', Icons.account_circle, null),
+  ];
+
+  int currentIndex = 0;
+  late BuildContext ctx;
+
+  // In case of pressing back
+  void onBackPress() {
+    if (Navigator.of(ctx).canPop()) {
+      Navigator.of(ctx).pop();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        toolbarHeight: 0,
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          statusBarIconBrightness: Brightness.dark,
+          statusBarColor: Colors.white,
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.blue[700],
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey[400],
+        currentIndex: currentIndex,
+        onTap: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        items: itemsNav.map((BottomNav d) {
+          return BottomNavigationBarItem(
+            icon: Icon(d.icon),
+            label: d.title,
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
